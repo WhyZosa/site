f35bf412d014c30a3f39edb76c61e09527cd8be3
WhyZosa <zasimukd@mail.ru>
2024-09-06 01:13:11 +0300

    site

--- myproject/mainapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('registration/', views.registration_view, name='registration'),
-    path('login/', views.login_view, name='login'),
-    path('welcome/', views.welcome_view, name='welcome'),
-]
